16ff6fdf7630c198d02a7100e9096f9a9c4939c3
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-11-19 02:56:58 +0000

    começando o projeto

--- src/models/funcionario.py
+++
@@ -1,7 +0,0 @@
-from src.models.pessoa import Pessoa
-
-
-class Funcionario(Pessoa):
-
-    def __init__(self, nome=..., cpf=..., telefone=..., nascimento=..., endereco=..., senha=..., email=...):
-        super().__init__(nome, cpf, telefone, nascimento, endereco, senha, email)
